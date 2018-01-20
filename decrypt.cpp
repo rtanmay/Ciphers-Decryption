216c53ba080503c335edc82080f756e20d9f884a
rtanmay <rtanmay05@gmail.com>
2018-01-21 01:48:25 +0530

    added decrypt.cpp file

---
+++ decrypt.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	cout<<"Enter the maximum size of the cipher text:";
+	int n;
+	cin>>n;
+	char CT[n],c;
+	s
+	//check for caesar first
+	c=caesar(CT);
+	if(c=='y')
+	{
+
+	}
+
+}
